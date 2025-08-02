421bfb0df21c8750b2f8e038e404a7a4650e57d3
Priyankshu-07 <priyankshusinha98@gmail.com>
2025-08-03 00:25:40 +0530

    made some changes for working

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/.env' });
+
 if (!process.env.MONGO_URI) {
   console.error('MONGO_URI not defined in .env');
   process.exit(1);
 }
+
 require('./db/mongo.js');
+
 const emailRoutes = require('./routes/emailRoutes.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
+
 app.get('/', (req, res) => res.send('VoltMail'));
 app.use('/api', emailRoutes);
+
 app.use((error, req, res, next) => {
   console.error('Error:', error);
-  res.status(500).json({ error: 'Something went wrong!' });
+  res.status(500).json({ error: 'Something went wrong' });
 });
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
